83304bdebced651d517da57eb7bb688ca8b5ab1d
David Zukor <zukord@gmail.com>
2019-02-24 22:23:12 -0500

    JS halfway done? Halfway done with instructions, at least.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,25 @@
 var topics=["Overwatch", "Skyrim", "Starcraft", "Red Alert", "Command and Conquer", "Tiberian Sun", "Heroes of the Storm", "Astroneer", "Oxygen Not Included", ];
 
 function gifTastic(){
-    for(var i=0; i < topics.length; i++)
-       //$("#vGameButtons").append("<button type=\"button\" class=\"btn\" id=\" + topics[i] + \" );
-       $("#vGameButtons").append("<button>" + topics[i] + "</button>").attr({
+    for(var i=0; i < topics.length; i++){
+       $("#vGameButtons").append($("<button>" + topics[i] + "</button>").attr({
           type: "button",
           class: "btn",
           id: topics[i] 
-       });
+       }));
+    }
+    $(".btn").click(function(){
+        $.ajax({
+            url:"https://api.giphy.com/v1/gifs/search?api_key=Bw7HvG3Hm5G3EpwYKn8HLLhzZrIVaNXN&q="+$(this).id+"&limit=10&rating=PG-13",
+            method: "GET"
+        }).then(function(response) {
+            for(var i=0; i < 10; i++){
+                $("#vGames").append($("<image> <br/>" + response.data[i].rating + "<br/> </image>").attr({
+                    src: response.data[i].images.fixed_height_still.url,
+                    class: "image"
+                }));
+            }
+        });
+    });
 }
 gifTastic();
